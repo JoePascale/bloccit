ad92fb90b87cd1baa64c91eca3a0afa877b6a8fb
JoePascale <joe.m.pascale@gmail.com>
2017-07-19 21:00:45 -0700

    assignment-18 completed, changed every 5th post title to demonstrate CRUD basics

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      index % 5 == 0 ? post.title = "SPAM" : post.title
+    end
   end
 
   def show
