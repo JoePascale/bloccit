2e0dccbd7100eb44aac04c30609d8d36d62e3b83
JoePascale <joe.m.pascale@gmail.com>
2017-07-31 20:43:01 -0700

    assignment 24 completed, added callback for user name that capitalizes first and last

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,11 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+    it "should capitalize first and last names" do
+      user.name = "joe slick"
+      user.save
+      expect(user.name).to eq("Joe Slick")
+    end
   end
 
   describe "invalid user" do
